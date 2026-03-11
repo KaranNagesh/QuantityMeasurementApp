9e5b8d18f68d85896b1916b1dd2521c382a95c28
KaranNagesh <Karannagesh86@gmail.com>
2026-03-12 01:34:34 +0530

    [Karan] : implemented UC15 feature of N-Tier Architecture Refactoring for quantity Measurement Application

---
+++ src/main/java/com/apps/quantitymeasurement/exception/InvalidUnitMeasurementException.java
@@ -0,0 +1,13 @@
+package com.apps.quantitymeasurement.exception;
+
+
+public class InvalidUnitMeasurementException extends RuntimeException{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public InvalidUnitMeasurementException(String msg) {
+		super(msg);
+	}
+}
